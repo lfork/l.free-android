34ec0d84df106a2889889d5f5dd2aa5cdb426810
L.Fork <986204478@qq.com>
2018-04-18 18:11:40 +0800

    1、解决了单图片上传出错的error 2、优化了大文件上传模块

--- app/src/main/java/com/lfork/a98620/lfree/data/source/remote/httpservice/Service.java
+++ app/src/main/java/com/lfork/a98620/lfree/data/source/remote/httpservice/Service.java
@@ -3,6 +3,7 @@ package com.lfork.a98620.lfree.data.source.remote.httpservice;
 import com.lfork.a98620.lfree.util.http.HttpLogger;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,6 +19,16 @@ public class Service {
 
     private static final String TAG = "Service";
 
+    private final static int CONNECT_TIMEOUT =40;
+    private final static int READ_TIMEOUT=30;
+    private final static int WRITE_TIMEOUT=30;
+//    public static OkHttpClient mOkHttpClient =
+//            new OkHttpClient.Builder()
+//                    .readTimeout(READ_TIMEOUT,TimeUnit.SECONDS)//设置读取超时时间
+//                    .writeTimeout(WRITE_TIMEOUT,TimeUnit.SECONDS)//设置写的超时时间
+//                    .connectTimeout(CONNECT_TIMEOUT,TimeUnit.SECONDS)//设置连接超时时间
+//                    .build();
+
     /**
      *  异步请求
      */
@@ -26,7 +37,13 @@ public class Service {
         HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor(new HttpLogger());
         logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client = new OkHttpClient().newBuilder().addNetworkInterceptor(logInterceptor).build();
+        OkHttpClient client = new OkHttpClient()
+                .newBuilder()
+                .addNetworkInterceptor(logInterceptor)
+                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)//设置读取超时时间
+                .writeTimeout(WRITE_TIMEOUT,TimeUnit.SECONDS)//设置写的超时时间  
+                .connectTimeout(CONNECT_TIMEOUT,TimeUnit.SECONDS)//设置连接超时时间
+                .build();
 
         Request request = new Request.Builder().url(url)
                 .post(requestBody).build();
