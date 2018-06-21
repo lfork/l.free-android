3bea1a615465441391f9fb59b4e4331db6eef06d
L.Fork <986204478@qq.com>
2018-06-22 00:23:43 +0800

    修复了之前由于使用全局Application Context导致GoodsDetailActivit启动失败的问题y

--- app/src/main/java/com/lfork/a98620/lfree/base/viewmodel/GoodsItemViewModel.java
+++ app/src/main/java/com/lfork/a98620/lfree/base/viewmodel/GoodsItemViewModel.java
@@ -7,13 +7,15 @@ import com.lfork.a98620.lfree.data.entity.Goods;
 import com.lfork.a98620.lfree.goodsdetail.GoodsDetailActivity;
 import com.lfork.a98620.lfree.util.Config;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by 98620 on 2018/4/14.
  */
 
 public class GoodsItemViewModel extends GoodsViewModel {
 
-
+    private WeakReference<Context> reference;
 
     public GoodsItemViewModel(Context context, Goods g) {
         super(context, g.getId());
@@ -22,12 +24,16 @@ public class GoodsItemViewModel extends GoodsViewModel {
         imagePath.set(Config.ServerURL + "/image" + g.getCoverImagePath() ); //
         publishDate.set(g.getPublishDate());
         setCategoryId(g.getCategoryId());
+        reference = new WeakReference<>(context);
     }
 
     public void onClick(){
-        Intent intent = new Intent(context, GoodsDetailActivity.class);
-        intent.putExtra("goods_id", getId());
-        intent.putExtra("category_id", getCategoryId());
-        context.startActivity(intent);
+
+        if (reference != null && reference.get() != null) {
+            Intent intent = new Intent(context, GoodsDetailActivity.class);
+            intent.putExtra("goods_id", getId());
+            intent.putExtra("category_id", getCategoryId());
+            reference .get().startActivity(intent);
+        }
     }
 }
